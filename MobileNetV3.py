c3c22106ea0cbe8ea09c480af3a6701667d13c9e
Jaemin <jaemin.lee.email@gmail.com>
2019-10-29 20:17:28 +0900

    fix: fixed error

--- MobileNetV3.py
+++ MobileNetV3.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def hswish(x) :
+def hswish_fn(x):
     x = x * (F.relu6(x + 3) / 6)
     return x
 
@@ -14,12 +14,12 @@ class bottleneck(nn.Module):
 
         self.activateFn = F.relu
         if hswish:
-            self.activateFn = hswish
+            self.activateFn = hswish_fn
 
         self.shortcut = nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=stride, padding=0, bias=False)
         self.bn = nn.BatchNorm2d(out_channel)
 
-        self.conv1 = nn.Conv2d(in_channel, expand_channel, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv1 = nn.Conv2d(in_channel, expand_channel, kernel_size=1, stride=1, padding=kernel_size//2, bias=False)
         self.bn1 = nn.BatchNorm2d(expand_channel)
 
         self.conv2 = nn.Conv2d(expand_channel, expand_channel, kernel_size=kernel_size, stride=stride, padding=0, groups=expand_channel, bias=False)
@@ -30,27 +30,27 @@ class bottleneck(nn.Module):
 
         if self.se:
             self.pooling = nn.AdaptiveAvgPool2d(1)
-            self.fc1 = nn.Linear(expand_channel, expand_channel//4)
-            self.fc2 = nn.Linear(expand_channel//4, expand_channel)
+            self.fc1 = nn.Conv2d(expand_channel, expand_channel//4, kernel_size=1, stride=1, padding=0, bias=False)
+            self.fc2 = nn.Conv2d(expand_channel//4, expand_channel, kernel_size=1, stride=1, padding=0, bias=False)
             self.hsigmoid = nn.Hardtanh(0, 1)
 
     def forward(self, x):
         shortcut = self.bn(self.shortcut(x))
-
+        
         x = self.activateFn(self.bn1(self.conv1(x)))
         x = self.activateFn(self.bn2(self.conv2(x)))
-
+        
         if self.se:
             x_se = self.pooling(x)
             x_se = self.fc1(x_se)
             x_se = F.relu(x_se)
             x_se = self.fc2(x_se)
             x_se = self.hsigmoid(x_se)
-
-            x = torch.matmul(x, x_se)
+            
+            x = x * x_se
+            
 
         x = self.activateFn(self.bn3(self.conv3(x)))
-
         x = x + shortcut
 
         return x
@@ -58,7 +58,7 @@ class bottleneck(nn.Module):
 class MobileNetV3Small(nn.Module):
     def __init__(self, num_classes=1000):
         super(MobileNetV3Small, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=0, bias=False)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
 
         self.bneck1 = bottleneck(in_channel=16, out_channel=16, expand_channel=16, kernel_size=3, stride=2, hswish=False, se=True)
@@ -74,11 +74,11 @@ class MobileNetV3Small(nn.Module):
         self.bneck11 = bottleneck(in_channel=96, out_channel=96, expand_channel=576, kernel_size=5, stride=1, hswish=True, se=True)
 
         self.conv2 = nn.Conv2d(96, 576, kernel_size=1, stride=1, padding=0, bias=False)
-        self.bn2 = nn.BatchNorm2d(960)
+        self.bn2 = nn.BatchNorm2d(576)
 
         self.pooling_se = nn.AdaptiveAvgPool2d(1)
-        self.fc1_se = nn.Linear(576, 576//4)
-        self.fc2_se = nn.Linear(576//4, 576)
+        self.fc1_se = nn.Conv2d(576, 576//4, kernel_size=1, stride=1, padding=0, bias=True)
+        self.fc2_se = nn.Conv2d(576//4, 576, kernel_size=1, stride=1, padding=0, bias=True)
         self.hsigmoid_se = nn.Hardtanh(0, 1)
 
         self.pooling = nn.AvgPool2d(7)
@@ -87,7 +87,7 @@ class MobileNetV3Small(nn.Module):
         self.conv4 = nn.Conv2d(1280, num_classes, kernel_size=1, stride=1, padding=0, bias=False)
 
     def forward(self, x):
-        x = hswish(self.bn1(self.conv1(x)))
+        x = hswish_fn(self.bn1(self.conv1(x)))
         x = self.bneck1(x)
         x = self.bneck2(x)
         x = self.bneck3(x)
@@ -99,26 +99,26 @@ class MobileNetV3Small(nn.Module):
         x = self.bneck9(x)
         x = self.bneck10(x)
         x = self.bneck11(x)
-  
-        x = hswish(self.bn2(self.conv2(x)))
+          
+        x = hswish_fn(self.bn2(self.conv2(x)))
         x_se = self.pooling_se(x)
-        x_se = self.fc1_se(x)
+        x_se = self.fc1_se(x_se)
         x_se = F.relu(x_se)
-        x_se = self.fc2_se(x)
+        x_se = self.fc2_se(x_se)
         x_se = self.hsigmoid_se(x)
 
-        x = torch.matmul(x, x_se)
+        x = x * x_se
         
         x = self.pooling(x)
-        x = hswish(self.conv3(x))
-        x = self.conv3(x)
+        x = hswish_fn(self.conv3(x))
+        x = self.conv4(x)
 
         return x
 
 class MobileNetV3Large(nn.Module):
     def __init__(self, num_classes=1000):
         super(MobileNetV3Large, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=0, bias=False)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
 
         self.bneck1 = bottleneck(in_channel=16, out_channel=16, expand_channel=16, kernel_size=3, stride=1, hswish=False, se=False)
@@ -145,7 +145,7 @@ class MobileNetV3Large(nn.Module):
         self.conv4 = nn.Conv2d(1280, num_classes, kernel_size=1, stride=1, padding=0, bias=False)
 
     def forward(self, x):
-        x = hswish(self.bn1(self.conv1(x)))
+        x = hswish_fn(self.bn1(self.conv1(x)))
         x = self.bneck1(x)
         x = self.bneck2(x)
         x = self.bneck3(x)
@@ -161,9 +161,9 @@ class MobileNetV3Large(nn.Module):
         x = self.bneck13(x)
         x = self.bneck14(x)
         x = self.bneck15(x)
-        x = hswish(self.bn2(self.conv2(x)))
+        x = hswish_fn(self.bn2(self.conv2(x)))
         x = self.pooling(x)
-        x = hswish(self.conv3(x))
-        x = self.conv3(x)
+        x = hswish_fn(self.conv3(x))
+        x = self.conv4(x)
 
         return x
